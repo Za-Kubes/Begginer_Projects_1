321cae8ccb1650284e87abf5197441a4c35cae22
Za Kubes <zevra56@gmail.com>
2026-06-12 00:27:25 +0100

    test 3 file

---
+++ test_3.py
@@ -0,0 +1,7 @@
+first_name = "Francis"
+last_name = "Victor"
+full_name = f"{first_name} {last_name}"
+print (full_name)
+print (full_name.upper())
+message = f"Hello {full_name}\t\nHope you are doing well" 
+print (message.title())
